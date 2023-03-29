e81717bf05610d76b42a0fbf106c2ed2d5edc652
zurctrebla <albertcruz@terra.com.br>
2023-03-28 23:33:05 -0300

    Fly Controller

--- routes/api.php
+++ routes/api.php
@@ -4,14 +4,30 @@ use App\Http\Controllers\Api\FlyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('/flies', FlyController::class);
-
-
 // 1º teste de rota.
 Route::get('/', function () {
     return response()->json(['message' => 'success']);
 });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('api')->get('/flies', [FlyController::class, 'index']);
+
+Route::middleware('api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/login', function(Request $request) {
+
+    $credentials = $request->only(['email', 'password']);
+
+    if(!$token = auth()->attempt($credentials)) {
+        abort(401, 'Unauthorized');
+    }
+
+    return response()->json([
+        'data' => [
+            'token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ],
+    ]);
 });
