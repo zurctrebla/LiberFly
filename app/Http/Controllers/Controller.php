b771671132dd66f3cb94cd142edb05a799af8437
zurctrebla <albertcruz@terra.com.br>
2023-03-29 14:52:24 -0300

    Swagger Install and initial config

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,7 +5,22 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-
+/**
+* @OA\Info(
+*      version="1.0.0",
+*      title="LiberFly",
+*      description="LiberFly Api Documentation",
+*      @OA\Contact(
+*          email="albertcruz@terra.com.br"
+*      )
+* )
+*
+* @OA\Server(
+*      url=L5_SWAGGER_CONST_HOST,
+*      description="LiberFly API"
+* )
+*
+*/
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
--- config/app.php
+++ config/app.php
@@ -210,6 +210,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        L5Swagger\L5SwaggerServiceProvider::class,
     ])->toArray(),
 
 ];
