820cf473adf0e1a78e19e0c626cee1da473f2bcf
zurctrebla <albertcruz@terra.com.br>
2023-03-28 23:23:04 -0300

    user observer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Models\Fly;
+use App\Models\User;
 use App\Observers\FlyObserver;
+use App\Observers\UserObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,5 +24,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Fly::observe(FlyObserver::class);
+        User::observe(UserObserver::class);
     }
 }
